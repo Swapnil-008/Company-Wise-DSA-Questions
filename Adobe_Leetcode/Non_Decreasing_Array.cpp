d00d6e2e9f410603a4c22da3f89aeeec94cf8dea
Swapnil <swapnilshingne155@gmail.com>
2025-01-09 23:19:07 +0530

    Non_Decreasing_Array.cpp

---
+++ Adobe_Leetcode/Non_Decreasing_Array.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    bool checkPossibility(vector<int>& nums)
+    {
+        int n = nums.size();
+        int count = 0;
+        for(int i = 1; i < n; i++)
+        {
+            if(nums[i-1] > nums[i])
+            {
+                if(count == 1)
+                {
+                    return false;
+                }
+                count++;
+                if(i == 1 || nums[i-2] <= nums[i])
+                {
+                    nums[i-1] = nums[i];
+                }
+                else{
+                    nums[i] = nums[i-1];
+                }
+            }
+        }
+        return true;
+    }
+};
